3bdeb4f5ed008149781cef18c9ba0ecbe918b0c2
DominicOH <dominic.halliwell@hotmail.com>
2023-03-07 18:46:42 +0000

    Further fixes

--- ProjectTools/__init__.py
+++ ProjectTools/__init__.py
@@ -2,6 +2,7 @@
 
 from .OpenBeds import get_nanopore_threeMod
 from .OpenBeds import get_nanopore_twoMod
-
 from .OpenBeds import get_tab
 from .OpenBeds import get_wgbs
+from .FeatIntersects import find_named_intersects
+from .FeatIntersects import find_type_intersects
