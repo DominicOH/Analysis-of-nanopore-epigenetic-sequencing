530ef86def81a7f0c782c332108f5e4554d07722
DominicOH <dominic.halliwell@hotmail.com>
2023-05-15 16:40:19 +0100

    Improvements to checkpointing

--- ProjectTools/__init__.py
+++ ProjectTools/__init__.py
@@ -1,8 +1,3 @@
 # __init__.py
 
-from .common import get_nanopore_threeMod
-from .common import get_nanopore_twoMod
-from .common import get_bismark
-from .common import get_wgbs
-from .common import get_nanopore_threeMod_wStrand
 from .common import *
