991a82634801f937931e67c1fb39bff44bbb4d50
DominicOH <dominic.halliwell@hotmail.com>
2023-08-15 14:51:36 +0100

    Changes to Multisite implementation

--- MultisiteTools.py
+++ MultisiteTools.py
@@ -40,6 +40,20 @@ class CpGRange(pr.PyRanges):
             "percentMeth_5hmC_Prom" : "percentMeth_5hmC_Nanopore"}, 
             errors="ignore")
         super().__init__(df, df)
+        self._raw_means = None
+
+    @property
+    def __raw_means(self):
+        """
+        This property is intended to be used for downstream applications - saving the mean modification % per CpG. 
+        """
+        list_of_cols = ["percentMeth_5mC_Bisulphite", "percentMeth_5hmC_Bisulphite", "percentMeth_5mC_Nanopore", "percentMeth_5hmC_Nanopore"]
+        raw_means = {}
+        for col in list_of_cols:
+            raw_means.update({
+                col : self.df[col].mean()
+            })
+        return raw_means
 
     def __annotate_with_multiple(self, annotation_dir_path):
         annotation_ref = featureRefPyRange(annotation_dir_path).unstrand()
@@ -68,9 +82,9 @@ class CpGRange(pr.PyRanges):
              "percentMeth_5hmC_Nanopore" : np.mean,
              "percentMeth_5hmC_Bisulphite" : np.mean,
              "Cluster" : "count"}
-             ).reset_index()
+             ).reset_index().rename(columns={"Cluster":"CpG_count"})
            
-        return grouped_df.rename(columns={"Cluster":"CpG_count"})
+        return Multisite(grouped_df, raw_means=self.__raw_means)
     
     def group_by_annotation(self, 
               intersect_with: str,
@@ -101,20 +115,29 @@ class CpGRange(pr.PyRanges):
         elif intersecting_on == "repeats":
             output_df = groupby_df.replace("-1", None).dropna()
 
-        return  Multisite(output_df.rename(columns={"Start_CpG" : "CpG_count"}))
+        return  Multisite(output_df.rename(columns={"Start_CpG" : "CpG_count"}), raw_means=self.__raw_means)
     
 class Multisite:
     """
-    Dataframe-type objects where CpG positions are grouped. Child classes contain additional functionality. Contains interface for relevant Seaborn plotting functions.  
+    PyRange objects where CpG positions are grouped.
     """
-    def __init__(self, df, cpg_threshold=1):
-        self.df = df.loc[df.loc[:, "CpG_count"].ge(cpg_threshold)]
+    def __init__(self, df, cpg_threshold=1, raw_means=None):
+        self._df = df.loc[df.loc[:, "CpG_count"].ge(cpg_threshold)]
+        self._raw_means = raw_means
+
+    @property
+    def df(self):
+        return self._df
     
-    def __ratioToMean(self, column: str):
-        new_col = self.df[column].divide(self.df[column].mean())
+    @property
+    def raw_means(self):
+        return self._raw_means
+    
+    def __calculate_ratio_to_mean(self, column: str):
+        new_col = self.df[column].divide(self.raw_means[column])
         return new_col
     
-    def __log2RatioWrapper(self, column: str, include_zeros=False):
+    def __calculate_log2_difference(self, column: str, include_zeros=False):
         if include_zeros:
             add = 1
         else: 
@@ -122,10 +145,10 @@ class Multisite:
         
         with np.errstate(divide="ignore"):
             log2_col = np.log2(
-                np.add(self.__ratioToMean(column), add))
+                np.add(self.__calculate_ratio_to_mean(column), add))
         return log2_col
 
-    def enrichmentComparison(self, include_zeros=False):
+    def enrichment_over_mean(self, include_zeros=False):
         """
         Provides additional columns with log_2 scale scores showing enrichment relative to the mean for 5mC and 5hmC. 
 
@@ -133,10 +156,12 @@ class Multisite:
         """
         df = self.df.copy()
         df = df.assign(
-            log2enrichment_5mC_Nanopore=self.__log2RatioWrapper("percentMeth_5mC_Nanopore", include_zeros), 
-            log2enrichment_5mC_Bisulphite=self.__log2RatioWrapper("percentMeth_5mC_Bisulphite", include_zeros), 
-            log2enrichment_5hmC_Nanopore=self.__log2RatioWrapper("percentMeth_5hmC_Nanopore", include_zeros),            
-            log2enrichment_5hmC_Bisulphite=self.__log2RatioWrapper("percentMeth_5hmC_Bisulphite", include_zeros)
+            log2enrichment_5mC_Nanopore=self.__calculate_log2_difference("percentMeth_5mC_Nanopore", include_zeros), 
+            log2enrichment_5mC_Bisulphite=self.__calculate_log2_difference("percentMeth_5mC_Bisulphite", include_zeros), 
+            log2enrichment_5hmC_Nanopore=self.__calculate_log2_difference("percentMeth_5hmC_Nanopore", include_zeros),            
+            log2enrichment_5hmC_Bisulphite=self.__calculate_log2_difference("percentMeth_5hmC_Bisulphite", include_zeros)
             )
         
-        return df
+        return Multisite(df, raw_means=self.raw_means)
+    
+
