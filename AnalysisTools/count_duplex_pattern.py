4bd34f2aaaa4ae872716e2c00fbc951339e27f7e
DominicOH <dominic.halliwell@hotmail.com>
2024-02-23 15:50:46 +0000

    Update: Add script to count patterns in duplex reads
    New script counts CpG modification patterns and returns the normalised proportion of reads with a given pattern of homo-/hetero-modification.

---
+++ AnalysisTools/count_duplex_pattern.py
@@ -0,0 +1,44 @@
+import argparse
+import pandas as pd
+import concurrent.futures
+import os
+from helpers import timer
+
+def count_patterns(path):
+    print(f"Reading patterns in {path}...")
+    pattern_df = pd.read_table(path, sep="\t", 
+                            names=["Chromosome", "Start", "End", "Pattern", "readCount", "D0", "D1", "D2", "D3", "D4", "percentPattern", "N_Pattern", "N_Canonical", "N_Other", "D5", "D6", "D7", "D8"],
+                            usecols=["Chromosome", "Start", "End", "Pattern", "readCount", "N_Pattern"])
+    
+    pattern_count = pattern_df["Pattern"].value_counts(normalize=True).reset_index()
+
+    outpath = outdir + os.path.basename(path) + ".duplex_patterns.tsv"
+
+    print(f"Done. Saving to {outpath}.")
+    return pattern_count.to_csv(outpath, sep="\t", index=False)
+
+@timer
+def main(paths):
+    if type(paths) == list:
+        with concurrent.futures.ThreadPoolExecutor(len(paths)) as tpe:
+            multiple_hemi_pileups = tpe.map(count_patterns, paths)
+            return multiple_hemi_pileups
+    else:
+        return count_patterns(paths)
+
+##### Main function #####
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(
+                        prog = "count_reads_dx",
+                        description = "Reads one or more bamfiles and counts the values associated with the DX tag.")
+    parser.add_argument("filenames", nargs="+")
+    parser.add_argument("-o ", "--outdir", type=str, required=False) 
+
+    args = parser.parse_args()
+
+    if args.outdir:
+        global outdir
+        outdir = args.outdir
+    
+    main(args.filenames)
