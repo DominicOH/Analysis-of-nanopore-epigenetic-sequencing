8d7e97b7b2959398580177d6a1d5dc55e70090f5
DominicOH <dominic.halliwell@hotmail.com>
2024-02-27 14:42:06 +0000

    Update: Improve figure look and clarity
    Changedcolorpalette to Greens. Change e. from barplot to box. Fix e. to percentage difference from median.

--- compare_coverage.py
+++ compare_coverage.py
@@ -9,7 +9,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 import seaborn as sns
-from AnalysisTools import common, read_modbed
+from AnalysisTools import common
 from AnalysisTools.annotation_features import annotate
 from AnalysisTools.helpers import timer
 import concurrent.futures
@@ -49,9 +49,22 @@ def fetch_data(dry_run: bool, split_biorep=False, **kwargs):
         bio_reps = ["Nanopore", "Nanopore"]
 
     with concurrent.futures.ThreadPoolExecutor(4) as ppe:
-        all_futures = [ppe.submit(common.openReps, path, insert_cols={"Technique" : bio_rep}, **kwargs) for path, bio_rep in zip([cbm2_path, cbm3_path], bio_reps)]
-        all_futures.append(ppe.submit(common.openReps, tab_path, insert_cols={"Technique" : "TAB"}, modbase="5hmC", **kwargs))
-        all_futures.append(ppe.submit(common.openReps, oxbs_path, insert_cols={"Technique" : "oxBS"},  modbase="5mC", **kwargs))
+        all_futures = [ppe.submit(common.openReps, path, 
+                                  insert_cols={"Technique" : bio_rep},
+                                  quiet=False, 
+                                  **kwargs) for path, bio_rep in zip([cbm2_path, cbm3_path], bio_reps)]
+        all_futures.append(ppe.submit(common.openReps, 
+                                      tab_path, 
+                                      insert_cols={"Technique" : "TAB"}, 
+                                      modbase="5hmC",
+                                      quiet=False, 
+                                      **kwargs))
+        all_futures.append(ppe.submit(common.openReps, 
+                                      oxbs_path, 
+                                      insert_cols={"Technique" : "oxBS"},  
+                                      modbase="5mC",
+                                      quiet=False, 
+                                      **kwargs))
         future_dfs = [onlyAutosomal(future.result()) for future in all_futures]
 
     return future_dfs
@@ -60,8 +73,15 @@ def make_histogram(df, ax, color, **kwargs):
     return sns.histplot(df, x="readCount", ax=ax, color=color, **kwargs)  
 
 def annotated_coverage(df, feature_dir_path):
-    annotated_df = annotate(df, feature_dir_path)
-    return annotated_df.assign(readCount_vs_avg = lambda df: np.log2(df["readCount"]/df["readCount"].mean()))
+    print("Annotating...")
+    df = annotate(df, feature_dir_path)
+    median = df["readCount"].median()
+
+    df = df.assign(readCount_vs_avg = lambda df: round(((df["readCount"]-median)/median)*100, ))
+    df["readCount_vs_avg"] = pd.to_numeric(df["readCount_vs_avg"], downcast="integer")
+    
+    print(f"Annotated {len(df)} rows.")
+    return df.drop(columns=["Chromosome", "Start", "End", "Start_Feature", "End_Feature", "Strand", "readCount"])
 
 @timer
 def fig_main(dry_run):
@@ -77,6 +97,7 @@ def fig_main(dry_run):
                                                                     "Replicate", 
                                                                     "Technique"])
     cbm3 = cbm3.replace(["Rep. 1", "Rep. 2"], ["Rep. 3", "Rep. 4"])
+    print("Completed reading data.")
     
     autosomal_dataframes = pd.concat([cbm2, cbm3, tab, oxbs])
     LEN_UNION = len(pr.PyRanges(autosomal_dataframes).merge(False, slack=-1)) # len of union of covered sites saved for later
@@ -92,9 +113,10 @@ def fig_main(dry_run):
 
     ax4 = fig.add_subplot(gs[1, 0])
 
+    print("Making barplot of coverage breadth by replicate...")
     sns.barplot(coverage_by_rep, 
                 x="Technique", y="Proportion_of_union", 
-                hue="Technique", palette=sns.color_palette("BuGn_r", 4)[1:], 
+                hue="Technique", palette=sns.color_palette("Greens_r", 4)[1:], 
                 order=["oxBS", "TAB", "Nanopore"], 
                 errorbar=("sd", 1), err_kws={
                     "linewidth" : 0.8, 
@@ -102,6 +124,7 @@ def fig_main(dry_run):
                     },
                 capsize=0.5,
                 ax=ax4)
+    print("Done...")
     
     ax4.set_ylabel("Percentage of all\ncovered sites")
     ax4.set_xlabel(None)
@@ -113,14 +136,17 @@ def fig_main(dry_run):
     ax2 = fig.add_subplot(gs[0, 1])
     ax3 = fig.add_subplot(gs[0, 2])
 
+    print("Making histograms of coverage depth by replicate...")
     with concurrent.futures.ThreadPoolExecutor(3) as hist_executor:
         hist_futures = [hist_executor.submit(make_histogram, df, ax, color, stat="percent", multiple="dodge", binrange=(0, 32), binwidth=2) for df, ax, color in zip(
             [oxbs, tab, pd.concat([cbm2, cbm3])],
             [ax1, ax2, ax3],
-            [sns.color_palette("BuGn", 4)[1], sns.color_palette("BuGn", 4)[2], sns.color_palette("BuGn", 4)[3]])]
+            [sns.color_palette("Greens", 4)[1], sns.color_palette("Greens", 4)[2], sns.color_palette("Greens", 4)[3]])]
+                
         for future in hist_futures:
-            future.result() 
-  
+            future.result()
+    print("Done...")
+          
     ax1.set_title("oxBS-seq")
     ax2.set_title("TAB-seq")
     ax3.set_title("Nanopore")
@@ -134,37 +160,46 @@ def fig_main(dry_run):
 
     ax5 = fig.add_subplot(gs[1, 1:])
 
-    feature_dir_path = "/mnt/data1/doh28/analyses/mouse_hydroxymethylome_analysis/feature_references/RefSeq_Select/"
+    feature_dir_path = "/mnt/data1/doh28/analyses/mouse_hydroxymethylome_analysis/feature_references/coverage_comparison_features/"
+    
+    print("Annotating datasets by genomic context...")
     with concurrent.futures.ProcessPoolExecutor(4) as annotation_executor:
         annotated_futures = [annotation_executor.submit(annotated_coverage, df, feature_dir_path) for df in [cbm2, cbm3, tab, oxbs]]
         annotated_df = pd.concat([future.result() for future in annotated_futures])
+        print(f"Done. Joined {len(annotated_df)} rows.")
 
     annotated_df = annotated_df.replace(
         ["intergenic", "genes", "intron", "cds", "1kbPromoter"], 
-        ["Intergenic", "Whole gene", "Intron", "CDS", "1kb Promoter"])
+        ["Intergenic", "Genic", "Intron", "CDS", "Promoter"])
 
     annotated_df["feature_type"] = pd.Categorical(annotated_df["feature_type"], 
-                                                  categories=["Intergenic", "Intron", "CDS", "1kb Promoter", "Whole gene"], 
+                                                  categories=["Intergenic", "Genic", "Intron", "CDS", "Promoter", "CGI"], 
                                                   ordered=True)
+    
+    print("Making boxplot of coverage by context...")
 
-    sns.barplot(annotated_df, 
+    ax5.axhline(0, ls="--", c="k", lw=0.8, label="Median")
+
+    sns.boxplot(annotated_df, 
                 x="feature_type", 
                 y="readCount_vs_avg", 
                 hue="Technique",
-                palette=sns.color_palette("BuGn_r", 4)[1:], 
-                errorbar=("sd", 1),
-                err_kws={"linewidth" : 0.8,
-                         "solid_capstyle" : "butt"},
-                capsize=0.5,
+                gap=0.2,
+                showfliers=False,
+                palette=sns.color_palette("Greens_r", 4)[1:], 
+                linewidth=0.8,
                 ax=ax5)
+    
+    print("Done...")
+    ax5.set_ylabel("Difference to\nmedian depth (%)")
+    ax5.yaxis.set_ticks(np.arange(-100, 400, 100))
 
-    ax5.axhline(0, linewidth=0.8, color="k", ls=":")
-    ax5.set_ylabel(f"Depth difference\nto mean (Log$_2$)")
     ax5.set_xlabel(None)
-    ax5.tick_params("x", labelrotation=15)
+    ax5.tick_params("x")
 
-    sns.move_legend(ax5, "lower center", bbox_to_anchor=(.5, 1), ncol=3, title=None, frameon=False)
-
+    sns.move_legend(ax5, "lower center", bbox_to_anchor=(.5, 1), 
+                    ncols=3, title=None, frameon=False, alignment="center")
+    
     for index, ax in enumerate([ax4, ax5]):
         ax.set_title(string.ascii_lowercase[index+3], fontdict={"fontweight" : "bold"}, loc="left")
 
@@ -196,3 +231,4 @@ if __name__=="__main__":
         dryrun = False
 
     fig_main(dryrun)
+    print("Completed.")
