a56f0c4871b354a1086446156990d062528de94f
DominicOH <dominic.halliwell@hotmail.com>
2024-03-19 10:06:07 +0000

    Fix: Read level pattern counts
    Fixe error where patterns were not grouped and thus counts were not counted correctly.

--- AnalysisTools/count_duplex_pattern.py
+++ AnalysisTools/count_duplex_pattern.py
@@ -10,11 +10,11 @@ def count_patterns(path):
                             names=["Chromosome", "Start", "End", "Pattern", "readCount", "D0", "D1", "D2", "D3", "D4", "percentPattern", "N_Pattern", "N_Canonical", "N_Other", "D5", "D6", "D7", "D8"],
                             usecols=["Chromosome", "Start", "End", "Pattern", "readCount", "N_Pattern"])
     
-    pattern_count = pattern_df["Pattern"].value_counts(normalize=True).reset_index()
+    pattern_count = pattern_df.groupby("Pattern")["N_Pattern"].sum().reset_index()
 
     outpath = outdir + os.path.basename(path) + ".duplex_patterns.tsv"
 
-    print(f"Done. Saving to {outpath}.")
+    print(f"Finished counting patterns at {len(pattern_df)} positions. Saving to {outpath}.")
     return pattern_count.to_csv(outpath, sep="\t", index=False)
 
 @timer
@@ -31,7 +31,7 @@ def main(paths):
 if __name__=="__main__":
     parser = argparse.ArgumentParser(
                         prog = "count_reads_dx",
-                        description = "Reads one or more bamfiles and counts the values associated with the DX tag.")
+                        description = "Reads one or more hemi-pileup files and tallies CpG modification states.")
     parser.add_argument("filenames", nargs="+")
     parser.add_argument("-o ", "--outdir", type=str, required=False) 
 
