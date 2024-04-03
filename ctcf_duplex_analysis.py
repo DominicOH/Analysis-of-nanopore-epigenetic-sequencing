67acbb403f6c78d63dbb6236d34c198794fba325
DominicOH <dominic.halliwell@hotmail.com>
2024-04-03 11:53:44 +0100

    Initial ctcf analysis script

---
+++ ctcf_duplex_analysis.py
@@ -0,0 +1,341 @@
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+import seaborn as sns
+import concurrent.futures
+import pyranges as pr
+import argparse
+from AnalysisTools.helpers import timer
+import string
+
+def read_duplex_modbed(path, test_run=True, replicate=None):
+    if test_run:
+        nrows = 100000
+    else:
+        nrows=None
+    
+    pattern_df = (pd.read_table(path, sep="\t", 
+                            names=["Chromosome", "Start", "End", "Pattern", "readCount", "D0", "D1", "D2", "D3", "D4", "percentPattern", "N_Pattern", "N_Canonical", "N_Other", "D5", "D6", "D7", "D8"],
+                            usecols=["Chromosome", "Start", "End", "Pattern", "readCount", "N_Pattern"],
+                            nrows=nrows)
+                            .pivot_table(values="N_Pattern", columns="Pattern", index=["Chromosome", "Start", "End"], fill_value=0)
+                            .reset_index())
+    print(f"Found {len(pattern_df)} sites in {path}")
+
+    if replicate:
+        pattern_df["Replicate"] = replicate
+
+    return pattern_df
+
+def merge_pattern(df):
+        df = (df.eval("CM = `-,m,C` + `m,-,C`")
+                .eval("CH = `-,h,C` + `h,-,C`")
+                .eval("MH = `m,h,C` + `h,m,C`")
+                .rename(columns={"-,-,C" : "CC",
+                                 "m,m,C" : "MM",
+                                 "h,h,C" : "HH",
+                                 "-,m,C" : "CM",
+                                 "m,-,C" : "MC",
+                                 "h,-,C" : "HC",
+                                 "-,h,C" : "CH",
+                                 "m,h,C" : "MH",
+                                 "h,m,C" : "HM"})
+                .eval("total_homometh = CC + MM + HH")
+                .eval("total_heterometh = CM + CH + MH")
+                .eval("readCount = total_homometh + total_heterometh"))
+        return PatternFrame(df)
+
+def read_merge(path, test_run=True, replicate=None):
+    return merge_pattern(read_duplex_modbed(path, test_run, replicate))
+
+class PatternFrame:
+    def __init__(self, pattern_df):
+        self.pattern_df = pattern_df
+    
+    def __query(self, expression, min_depth=1):
+        return PatternFrame(self.pattern_df.query(f"{expression} & readCount >= {min_depth}"))
+    
+    def __min_depth(self, min_depth=1):
+        return PatternFrame(self.pattern_df.query(f"readCount >= {min_depth}"))
+    
+    def hemi_ctcf_proportion(self, min_depth=1):
+        """
+        Indicates what proportion of primarily hemimodified sites overlap with CTCF binding sites.  
+        """
+        pf = self.__query("total_heterometh > total_homometh", min_depth)
+        intersecting = len(ctcf_intersect(pf))
+        non_intersecting = len(ctcf_intersect(pf, invert=True))
+                           
+        return [intersecting/(intersecting + non_intersecting), intersecting, non_intersecting]
+    
+    def homo_ctcf_proportion(self, min_depth=1):
+        """
+        Indicates what proportion of primarily homomodified sites overlap with CTCF binding sites.  
+        """
+        pf = self.__query("total_homometh > total_heterometh", min_depth)  
+        intersecting = len(ctcf_intersect(pf))
+        non_intersecting = len(ctcf_intersect(pf, invert=True))
+                           
+        return [intersecting/(intersecting + non_intersecting), intersecting, non_intersecting]
+    
+    def ctcf_hemi_proportion(self, min_depth):
+        """
+        Indicates what proportion of CTCF-overlapping sites are hemimodified. 
+        """
+        df = ctcf_intersect(self.__min_depth(min_depth))
+
+        homo = len(df.query("total_homometh > total_heterometh"))
+        hemi = len(df.query("total_heterometh > total_homometh"))
+            
+        return [hemi/(homo + hemi), homo, hemi]
+
+    def ctcf_homo_proportion(self, min_depth):
+        """
+        Indicates what proportion of CTCF-overlapping sites are hemimodified. 
+        """
+        df = ctcf_intersect(self.__min_depth(min_depth))
+
+        homo = len(df.query("total_homometh > total_heterometh"))
+        hemi = len(df.query("total_heterometh > total_homometh"))
+            
+        return [homo/(homo + hemi), f"Homo: {homo}", f"Hemi {hemi}"]
+    
+    def count_homomethylated(self):
+        counts_df = self.pattern_df.loc[:,("CC", "HH", "MM")].sum().reset_index(name="Count")
+        counts_df["Percentage"] = (counts_df["Count"]/counts_df["Count"].sum()).multiply(100)
+        counts_df = counts_df.replace(["CC", "MM", "HH"], ["C", "5mC", "5hmC"])        
+        return counts_df
+        
+    def quantify_homohemi(self):
+        df = self.pattern_df
+        summary = ((df[["total_homometh", "total_heterometh"]].sum()
+                   .div(df["readCount"].sum()).multiply(100))
+                   .reset_index(name="Percentage"))
+        
+        return summary
+    
+    def compare_motif_calls_ctcf(self):
+        df = self.pattern_df.loc[:, ("Chromosome", "Start", "End", "MC", "CM", "HC", "CH", "HM", "MH")]
+        df = df.loc[df.eval("(MC + CM + HC + CH + HM + MH) > 0")]
+
+        ctcf_intersect = (pr.PyRanges(df).join(ctcf_bs, apply_strand_suffix=True, suffix="_CTCF").as_df())           
+
+        # remove those with no hemimethlyation
+        ctcf_intersect = ctcf_intersect.melt(id_vars=["Chromosome", "Start", "End", "Strand_CTCF"], var_name="Pattern", value_vars=["MC", "CM", "HC", "CH", "HM", "MH"], value_name="Count")
+        ctcf_intersect = ctcf_intersect.loc[ctcf_intersect.eval("Count > 0")]
+
+        summary = ctcf_intersect.groupby(["Strand_CTCF", "Pattern"])["Count"].sum().reset_index()
+        summary["Percentage"] = summary["Count"].div(summary["Count"].sum()).multiply(100)
+
+        summary = pd.concat([summary, (summary.Pattern.str.split("", n=2, expand=True)
+                                .drop(columns=0)
+                                .rename(columns={1 : "pos_mod", 
+                                                    2 : "neg_mod"}))], axis=1)
+
+        summary["motif_mod"] = summary["pos_mod"].where(cond=(summary["Strand_CTCF"] == "+"), other=summary["neg_mod"])
+        summary["opp_mod"] = summary["neg_mod"].where(cond=(summary["Strand_CTCF"] == "+"), other=summary["pos_mod"])
+
+        summary = summary.groupby(["motif_mod", "opp_mod"]).sum(numeric_only=True).reset_index()
+        summary = summary.replace(["C", "M", "H"], ["C", "5mC", "5hmC"])
+
+        return  Summary(summary)
+    
+class CTCF_PatternFrame(PatternFrame):
+    def __init__(self, pattern_df):
+        self.pattern_df = ctcf_intersect(pattern_df)
+    
+class Summary:
+    def __init__(self, df):
+        self.df = df
+    
+    def quantify_ctcf_hemi_mods(self):
+        summary = self.df
+        summary = summary.melt(value_vars=["motif_mod", "opp_mod"], id_vars="Count")
+
+        summary = summary.groupby("value").sum(True).reset_index()
+        summary["Percentage"] = (summary["Count"].div(summary["Count"].sum()).multiply(100))
+
+        return  summary
+    
+    def ctcf_mod_patterns(self):
+        summary = self.df
+        summaries = [summary.groupby("motif_mod").get_group(mod) for mod in summary["motif_mod"].unique()]
+
+        return summaries
+
+# Loading JASPAR CTCF binding sites 
+ctcf_bs = pr.PyRanges(pd.read_table("feature_references/CTCF_mm39_jaspar_sorted.tsv")
+                    .query("qvalue < 0.05")
+                    .rename(columns={"seqnames" : "Chromosome", "start" : "Start", "end" : "End", "strand" : "Strand"}))
+
+def ctcf_intersect(pattern_df: PatternFrame, **intersect_kwargs):
+    return pr.PyRanges(pattern_df.pattern_df).intersect(ctcf_bs, strandedness=False, **intersect_kwargs).as_df()
+
+@timer
+def main(test_run=True, min_depth=1):
+    root_path = "data/duplex_data/"
+    files = ["cbm2/CBM_2_rep1.masked.bed", "cbm2/CBM_2_rep2.masked.bed",
+             "cbm3/CBM_3_rep1.sorted.bam.bed", "cbm3/CBM_3_rep2.sorted.bam.bed"]
+
+    file_paths = [root_path + file for file in files]
+
+    with concurrent.futures.ProcessPoolExecutor(len(file_paths)) as load_executor:
+        all_duplex_modbeds = [load_executor.submit(read_merge, path, test_run, replicate+1) for replicate, path in enumerate(file_paths)]
+        all_duplex_modbeds = [modbed.result() for modbed in all_duplex_modbeds]
+                              
+    fig = plt.figure(figsize=(89/25.4, 120/25.4), dpi=600, layout="constrained")
+
+    sns.set_style("ticks")
+    mpl.rc('font', size=5)
+
+    gs = GridSpec(4, 3, fig)
+
+    ax1 = fig.add_subplot(gs[:2, :2])
+    ax2 = fig.add_subplot(gs[0, 2]) 
+    ax3 = fig.add_subplot(gs[1, 2]) 
+    ax4 = fig.add_subplot(gs[2:4, :2]) 
+    ax5 = fig.add_subplot(gs[2, 2])
+
+    sgs = gs[3, 2].subgridspec(1, 3)
+
+    ax6 = fig.add_subplot(sgs[0, 0])
+    ax7 = fig.add_subplot(sgs[0, 1])
+    ax8 = fig.add_subplot(sgs[0, 2])
+
+    # ax10 = fig.add_subplot(gs[4, 2])
+
+    palette = {
+    "C" : "#e0f3db", 
+    "5mC" : "#a8ddb5", 
+    "5hmC" : "#43a2ca"
+           }
+
+    with concurrent.futures.ThreadPoolExecutor(4) as stat_executor:
+        homohemi_proportions = [stat_executor.submit(pf.quantify_homohemi) for pf in all_duplex_modbeds]
+        homohemi_proportions = pd.concat([(proportion.result().assign(Replicate = i)) for i, proportion in enumerate(homohemi_proportions)])
+    
+    print(homohemi_proportions)
+    homohemi_proportions = homohemi_proportions.replace(["total_homometh", "total_heterometh"], 
+                                                        ["Homo-\nmodified", "Hetero-\nmodified"])
+    sns.barplot(homohemi_proportions, 
+                x="Pattern", y="Percentage",
+                hue="Pattern", palette="BuGn",
+                errorbar=("sd", 1), err_kws={"lw" : .8}, capsize=.3,
+                width=0.6,
+                ax=ax2)
+    ax2.set_ylabel("% of CTCF\nCpG sites")
+
+    with concurrent.futures.ThreadPoolExecutor(4) as stat_executor:
+        homo_proportions = [stat_executor.submit(pf.count_homomethylated) for pf in all_duplex_modbeds]
+        homo_proportions = pd.concat([(proportion.result().assign(Replicate = i)) for i, proportion in enumerate(homo_proportions)])
+
+    sns.barplot(homo_proportions, 
+                x="Pattern", y="Percentage",
+                hue="Pattern", palette=palette,
+                order=["C", "5mC", "5hmC"],
+                errorbar=("sd", 1), err_kws={"lw" : .8}, capsize=.5,
+                ax=ax3)
+    
+    with concurrent.futures.ThreadPoolExecutor(4) as stat_executor:
+        hetero_mods_at_ctcf = [stat_executor.submit(pf.compare_motif_calls_ctcf) for pf in all_duplex_modbeds]
+        hetero_mods_at_ctcf = [summary.result() for summary in hetero_mods_at_ctcf]
+        hetero_mod_summary = pd.concat([summary.quantify_ctcf_hemi_mods()
+                                        .assign(Replicate = i) for i, summary in enumerate(hetero_mods_at_ctcf)])
+
+    sns.barplot(hetero_mod_summary, 
+        x="value", y="Percentage",
+        order=["C", "5mC", "5hmC"],
+        hue="value", palette=palette,
+        errorbar=("sd", 1), err_kws={"lw" : .8}, capsize=.5,
+        ax=ax5)
+
+    all_mods = pd.concat([df.df.assign(Replicate = i) for i, df in enumerate(hetero_mods_at_ctcf)])
+    c, m, h = [all_mods.groupby("motif_mod").get_group(mod) for mod in ["C", "5mC", "5hmC"]]
+
+    sns.barplot(c, 
+            x="motif_mod", y="Percentage",
+            hue_order=["5mC", "5hmC"],
+            hue="opp_mod", palette=palette,
+            errorbar=("sd", 1), err_kws={"lw" : .8}, capsize=.5,
+            width=0.6, 
+            legend=False,
+            ax=ax6)
+    ax6.sharey(ax7)
+    ax6.set_ylabel("% opposite")
+
+    sns.barplot(m, 
+                x="motif_mod", y="Percentage",
+                hue="opp_mod", palette=palette,
+                errorbar=("sd", 1), err_kws={"lw" : .8}, capsize=.5,
+                width=0.6, 
+                legend=False,
+                ax=ax7)
+
+    sns.barplot(h, 
+                x="motif_mod", y="Percentage",
+                hue="opp_mod", palette=palette,
+                errorbar=("sd", 1), err_kws={"lw" : .8}, capsize=.5,
+                width=0.6, 
+                legend=False,
+                ax=ax8)
+    ax8.sharey(ax7)
+    
+    sns.despine()
+
+    [ax.set_xlabel(None) for ax in [ax2, ax3, ax5, ax6, ax7, ax8]]
+    for ax in [ax7, ax8]:
+        ax.set_ylabel(None)
+        ax.tick_params("both", left=False, labelleft=False)
+        sns.despine(ax=ax, left=True)
+    # [sns.move_legend(ax, "upper center", frameon=False, ncols=2, title=None) for ax in [ax7, ax8, ax9]]
+
+    for i, ax in enumerate(fig.axes):
+        ax.set_title(string.ascii_lowercase[i], fontdict={"fontweight" : "bold"}, loc="left")
+
+    for ax in [ax1, ax4]:
+        sns.despine(ax=ax, bottom=True, left=True)
+        ax.tick_params("both", bottom=False, left=False, 
+                        labelbottom=False, labelleft=False)
+    
+    if test_run:
+        fig.savefig("plots/tests/ctcf_duplex_analysis.png")
+    else:
+        fig.savefig("plots/ctcf_duplex_analysis.png")
+        fig.savefig("plots/ctcf_duplex_analysis.svg")
+
+    #    homo_proportions = [stat_executor.submit(homo_ctcf_proportion_wrap, mb, min_depth) for mb in all_duplex_modbeds]
+    #    [print(proportion.result()) for proportion in homo_proportions]
+
+##### Main function #####
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(
+                        prog = "ctcf_duplex_analysis",
+                        description = "Analyses duplex modified basecalls.")
+    parser.add_argument("-t ", "--testrun", 
+                        action="store_true", 
+                        dest="test_run", 
+                        required=False,
+                        help="Whether a test output is produced.") 
+    parser.add_argument("--min_depth", 
+                        dest="min_depth", 
+                        default=1,
+                        required=False,
+                        help="Whether to filter the dataset by depth at CpG.")
+    parser.add_argument("--agg", 
+                        dest="agg", 
+                        action="store_true",
+                        default=False,
+                        required=False,
+                        help="Whether to aggregate replicates from the dataset.") 
+
+    args = parser.parse_args()
+
+    if args.test_run:
+        test_run = True
+    else: 
+        test_run = False
+
+    args = parser.parse_args()
+    main(test_run, args.min_depth, args.agg)    
