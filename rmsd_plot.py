4911654df575fee402d4cd4cf7946bdbff925c15
DominicOH <dominic.halliwell@hotmail.com>
2024-10-16 12:57:47 +0100

    Update RMSD calculations add visual
    New plotting script plots RMSD as a function of coverage depth for intra-assay and inter-assay CpG deviation

--- rmsd_plot.py
+++ rmsd_plot.py
@@ -5,38 +5,71 @@ import matplotlib as mpl
 import string
 
 def main():
-    ox_rmsds = pd.read_table("nanopore_vs_ox_rmsd.tsv").rename(columns={"Bisulphite_rep" : "oxBS-seq rep.",
-                                                                        "Comparison" : "Nanopore rep."})
-    tab_rmsds = pd.read_table("nanopore_vs_tab_rmsd.tsv").rename(columns={"Bisulphite_rep" : "TAB-seq rep.",
-                                                                        "Comparison" : "Nanopore rep."})
 
     mpl.rc('font', size=5)
 
-    fig, axes = plt.subplots(2, 1, figsize=(120/25.4, 89/25.4), dpi=600, layout="constrained")
-    ax1, ax2 = axes
+    fig, axes = plt.subplots(4, 1, figsize=(120/25.4, 120/25.4), dpi=600, layout="constrained")
+    ax1, ax2, ax3, ax4 = axes
 
-    sns.scatterplot(ox_rmsds, 
-                x="Depth", y="RMSD",
-                style="oxBS-seq rep.", hue="Nanopore rep.", 
-                palette="BuGn",
+    nanopore_ia_mc = pd.read_table("data/rmsd/nanopore_intrassay_5mC.tsv").assign(Modification = "5mC", Method="Nanopore")
+    nanopore_ia_hmc = pd.read_table("data/rmsd/nanopore_intrassay_5hmC.tsv").assign(Modification = "5hmC", Method="Nanopore")
+
+    tab_ia = pd.read_table("data/rmsd/tab_intrassay.tsv").assign(Modification = "5hmC", Method="TAB")
+    ox_ia = pd.read_table("data/rmsd/oxbs_intrassay.tsv").assign(Modification = "5mC", Method="oxBS")
+
+    all_iarmsd = (pd.concat([nanopore_ia_mc, nanopore_ia_hmc, ox_ia, tab_ia])
+                  .drop_duplicates(['Depth', 'Size', 'RMSD', 'Modification', 'Method']))
+
+    sns.lineplot(all_iarmsd, 
+                x="Depth", y="RMSD", 
+                errorbar=None,
+                style="Modification", 
+                hue="Method", palette="Accent", hue_order=["Nanopore", "TAB", "oxBS"],
                 ax=ax1)
+    
+    ax1.set_title("Intra-assay deviation")
 
-    sns.scatterplot(tab_rmsds, 
-                x="Depth", y="RMSD",
-                style="TAB-seq rep.", hue="Nanopore rep.",
-                palette="PuBuGn", 
+    sns.lineplot(all_iarmsd, 
+                x="Depth", y="Mean_X", 
+                style="Modification",
+                hue="Method", palette="Accent", hue_order=["Nanopore", "TAB", "oxBS"],
                 ax=ax2)
+    
+    ax2.set_ylabel("Mean CpG\nmodification (%)")
+    ax2.set_ylim(0)
 
-    ax1.set_title("5mC")
-    ax2.set_title("5hmC")   
+    ox_rmsds = pd.read_table("data/rmsd/nanopore_vs_ox_rmsd.tsv").rename(columns={"Other_rep" : "oxBS-seq rep.",
+                                                                                  "Comparison" : "Nanopore rep."})
+    tab_rmsds = pd.read_table("data/rmsd/nanopore_vs_tab_rmsd.tsv").rename(columns={"Other_rep" : "TAB-seq rep.",
+                                                                                    "Comparison" : "Nanopore rep."})
+    sns.lineplot(ox_rmsds, 
+                x="Depth", y="RMSD",
+                errorbar=None,
+                style="oxBS-seq rep.", hue="Nanopore rep.", 
+                palette="BuGn", 
+                ax=ax3)
+
+    sns.lineplot(tab_rmsds, 
+                x="Depth", y="RMSD",
+                errorbar=None,
+                style="TAB-seq rep.", hue="Nanopore rep.",
+                palette="PuBuGn",
+                ax=ax4)
+
+    ax3.set_title("5mC")
+    ax4.set_title("5hmC")   
 
     for i, ax in enumerate(axes):
-        sns.move_legend(ax, "lower left", bbox_to_anchor=(-.33, .15), frameon=False)
-        ax.set_xticks(range(5, 26, 5))
-        ax.set_ylim(0, 25)
+        ax.set_xticks(range(5, 16, 1))
+        sns.move_legend(ax, "lower left", bbox_to_anchor=(-.31, -.25), frameon=False) 
+        ax.set_xlim(5, 15)
         ax.set_xlabel("Minimum depth at CpG site")
-        ax.set_title(string.ascii_lowercase[i], fontweight="bold", loc="left")
-
+        ax.set_title(string.ascii_lowercase[i], fontweight="bold", loc="left", x=-.3)
+        ax.get_legend().set_in_layout(False)
+    
+    for ax in [ax1, ax3, ax4]:
+        ax.set_ylim(0, 25) 
+        ax.set_yticks(range(0, 26, 5))
     sns.despine()
     fig.savefig("plots/rmsd_plot.png")
     fig.savefig("plots/rmsd_plot.svg")
--- rmse_calculator.py
+++ rmse_calculator.py
@@ -1,63 +1,117 @@
-from sklearn.metrics import root_mean_squared_error
+from sklearn.metrics import root_mean_squared_error, median_absolute_error
 import pandas as pd
-import subprocess
 import concurrent.futures
 from AnalysisTools import common
 import numpy as np
 import argparse
 from AnalysisTools.helpers import timer
 
-def one_vs_one_rmsd(nanopore_df: pd.DataFrame, bis_df: pd.DataFrame, mod: str, depth_levels: list[int]):
-    merge = (nanopore_df.merge(bis_df, 
+"""
+This script is used to calculate Root Mean Square Deviation between various permutations of the Nanopore and Bisulphite data. 
+
+The dataframe inputs used are produced using AnalysisTools/read_modbed using a Modkit bedMethyl or Bismark zero.cov output. 
+"""
+
+def levelled_rmsd(merged_dataframe: pd.DataFrame, mod, depth_levels: range, intraassay: bool = False):
+    level_dicts = []
+    comparison = ""
+    for level in depth_levels:
+        level_df = merged_dataframe.loc[merged_dataframe.eval(f"readCount_x >= {level} & readCount_y >= {level}")]
+
+        if len(level_df) != 0:
+            if not intraassay:
+                x = level_df[f"percentMeth_{mod}"].to_numpy()
+                y = level_df["percentMeth_mod"].to_numpy()
+                rmse = root_mean_squared_error(x, y)
+                mad = median_absolute_error(x, y)
+            else:
+                x = level_df[f"percentMeth_{mod}_x"].to_numpy()
+                y = level_df[f"percentMeth_{mod}_y"].to_numpy()
+                rmse = root_mean_squared_error(x, y)
+                mad = median_absolute_error(x, y)
+            
+        else:
+            pass
+        
+        level_dict = {
+            "Depth" : level,
+            "Size" : len(level_df),
+            "RMSD" : rmse, 
+            "MAD" : mad,
+            "Mean_X" : x.mean(),
+            "Mean_Y" : y.mean(),
+        }   
+        if intraassay:
+            if not comparison:
+                comparison = merged_dataframe.get("Comparison")[0]
+            else:
+                level_dict.update({
+                    "Comparison" : comparison
+                })
+
+        del level_df
+        level_dicts.append(level_dict)
+        
+        if debug:
+            print(level_dict)
+
+    out_df = pd.DataFrame.from_records(level_dicts)
+
+    return out_df
+
+def one_vs_one(nanopore_df: pd.DataFrame, other_df: pd.DataFrame, mod: str, depth_levels: range, intrassay: bool = False):
+    merge = (nanopore_df.merge(other_df, 
                                on=["Chromosome", "Start", "End"], 
                                how="inner")
-                        .drop(columns=["Chromosome", "Start", "End"]))
+                        .drop(columns=["Chromosome", "Start", "End", "N_mod", "N_5hmC", "N_5mC"], errors="ignore"))
     
-    rmses = []
-    sizes = []
-    means_n = []
-    means_b = []
+    ovo_result = levelled_rmsd(merge, mod, depth_levels, intrassay)
 
-    for level in depth_levels:
-        merge = merge.query(f"readCount_x >= {level} & readCount_y >= {level}")
-        try: 
-            rmse = root_mean_squared_error(merge[f"percentMeth_{mod}"], merge[f"percentMeth_mod"])
-        except:
-            if len(merge) == 0:
-                # print("Too few matched positions.")
-                pass
+    if debug:
+        print(ovo_result)
 
-        rmses.append(rmse)
-        sizes.append(len(merge))
-        means_n.append(merge[f"percentMeth_{mod}"].mean())
-        means_b.append(merge[f"percentMeth_mod"].mean())
+    return  ovo_result  
 
-    ovo_result = pd.DataFrame({
-        "Depth" : depth_levels,
-        "Size" : sizes,
-        "RMSD" : rmses, 
-        "Nanopore_mean" : means_n,
-        "Bisulphite_mean" : means_b
-    })
-
-    return  ovo_result            
-
-def one_vs_many_rmsd(nano_df: pd.DataFrame, bs_dfs: list[pd.DataFrame], mod: str, depth_levels: list[int]):
+def one_vs_many(nano_df: pd.DataFrame, other_dfs: list[pd.DataFrame], mod: str, depth_levels: range, other_rep_names, intraassay: bool = False):
     """
     Aggregates the result of several one-vs-one RMSD calculations. 
     """
-    with concurrent.futures.ThreadPoolExecutor(len(bs_dfs)) as ovo_tpe:
-        futures = [ovo_tpe.submit(one_vs_one_rmsd, nano_df, bs_df, mod, depth_levels) for bs_df in bs_dfs]
-        one_vs_many_result = pd.concat([rmse.result().assign(Bisulphite_rep = i+1) for i, rmse in enumerate(futures)])
+    if type(other_rep_names) == list[int]:
+        other_rep_names = [i+1 for i in other_rep_names]
+    with concurrent.futures.ThreadPoolExecutor(len(other_dfs)) as ovo_tpe:
+        futures = [ovo_tpe.submit(one_vs_one, nano_df, other_df, mod, depth_levels, intraassay) for other_df in other_dfs]
+        one_vs_many_result = pd.concat([rmse.result().assign(Other_rep = i) for i, rmse in zip(other_rep_names, futures)])
 
     return one_vs_many_result
 
-def many_vs_many_rmsd(nano_dfs: list[pd.DataFrame], bs_dfs: list[pd.DataFrame], mod: str, depth_levels: list[int]):
+def many_vs_many_intrassay(compare_dfs: list[pd.DataFrame], mod: str, depth_levels: range, rep_names: list = None):
+    if rep_names is None:
+        rep_names = [i+1 for i in range(len(compare_dfs))]
+
+    if debug:
+        processors = 1
+    else: 
+        processors = len(compare_dfs)
+
+    with concurrent.futures.ThreadPoolExecutor(processors) as mvm_executor:
+        mvm_futures = [mvm_executor.submit(one_vs_many, 
+                                           df_a.assign(Comparison = i), compare_dfs, mod, depth_levels, rep_names, intraassay=True) 
+                       for i, df_a in zip(rep_names, compare_dfs)]
+        mvm_result = pd.concat([future.result() for future in mvm_futures])
+    
+    mvm_result = (mvm_result.replace({"RMSD" : 0}, np.nan)
+                  .dropna()
+                  .drop_duplicates(["Depth", "Size", "RMSD"]))
+                  
+    return mvm_result 
+
+def many_vs_many_interassay(nano_dfs: list[pd.DataFrame], bs_dfs: list[pd.DataFrame], mod: str, depth_levels: list[int]):
     """
     Aggregates the result of several one-vs-many RMSD calculations. Each one-vs-many is numbered. 
     """
-    with concurrent.futures.ProcessPoolExecutor(len(nano_dfs)) as mvm_executor:
-        ovm_futures = [mvm_executor.submit(one_vs_many_rmsd, nano_df, bs_dfs, mod, depth_levels) for nano_df in nano_dfs]
+    rep_names = [i for i in range(len(bs_dfs))]
+    with concurrent.futures.ThreadPoolExecutor(len(nano_dfs)) as mvm_executor:
+        ovm_futures = [mvm_executor.submit(one_vs_many, nano_df, bs_dfs, mod, depth_levels, rep_names) for nano_df in nano_dfs]
         mvm_result = pd.concat([ovm.result().assign(Comparison = i) for i, ovm in zip(("CBM2_1", "CBM2_2", "CBM3_1", "CBM3_2"), ovm_futures)])
 
     return mvm_result
@@ -80,33 +134,41 @@ def load_and_calculate_percentages(path, cols: list[str]):
 
 @timer
 def main():
-    nano_path = "data/modbases/modbeds/nanopore/"
+    nano_path = "data/modbases/modbeds/nanopore_base5/"
     tab_path = "data/modbases/modbeds/tab/"
     oxbs_path = "data/modbases/modbeds/oxbs/"
 
     paths = [nano_path, tab_path, oxbs_path]
     cols = [["readCount", "N_mC", "N_hmC"], ["readCount", "N_mod"], ["readCount", "N_mod"]]
 
+    print("Loading datasets")
     with concurrent.futures.ProcessPoolExecutor(3) as fetch_executor:
         data_future = [fetch_executor.submit(load_and_calculate_percentages, path, cols) for path, cols in zip(paths, cols)]
         nano_mb, tab_mb, oxbs_mb = [future.result() for future in data_future]
     
-    # print(f"Interassay RMSE between Nanopore 5mC: {nested_mean(rmse_comparer(nano_mb, nano_mb, '5mC', '5mC'))}")
-    # print(f"Interassay RMSE between Nanopore 5hmC: {nested_mean(rmse_comparer(nano_mb, nano_mb, '5hmC', '5hmC'))}")
+    print("Performing many-vs-many RMSD calculations for Nanopore vs. Nanopore")
+
+    level_range = range(4, 25, 1)
+    nano_iassay_mc = many_vs_many_intrassay(nano_mb, "5mC", level_range, ("CBM2_1", "CBM2_2", "CBM3_1", "CBM3_2"))
+    nano_iassay_hmc = many_vs_many_intrassay(nano_mb, "5hmC", level_range, ("CBM2_1", "CBM2_2", "CBM3_1", "CBM3_2"))
+    nano_iassay_mc.to_csv("data/rmsd/nanopore_intrassay_5mC.tsv", index=False, sep="\t")
+    nano_iassay_hmc.to_csv("data/rmsd/nanopore_intrassay_5hmC.tsv", index=False, sep="\t")
+    
+    print("Performing many-vs-many RMSD calculations for Bisulphite vs. Bisulphite")
+    tab_iassay = many_vs_many_intrassay(tab_mb, "mod", level_range)
+    tab_iassay.to_csv("data/rmsd/tab_intrassay.tsv", index=False, sep="\t")
+    ox_iassay = many_vs_many_intrassay(oxbs_mb, "mod", level_range)
+    ox_iassay.to_csv("data/rmsd/oxbs_intrassay.tsv", index=False, sep="\t")
 
     print("Performing many-vs-many RMSD calculations for Nanopore vs. oxBS")
-    ox_result = many_vs_many_rmsd(nano_mb, oxbs_mb, '5mC', [i for i in range(5, 25, 1)])
+    ox_result = many_vs_many_interassay(nano_mb, oxbs_mb, '5mC', level_range)
     print(f"Average RMSD at 10x depth: {ox_result.loc[ox_result['Depth']==10, 'RMSD'].mean()}")
-    ox_result.to_csv("nanopore_vs_ox_rmsd.tsv", index=False, sep="\t")
+    ox_result.to_csv("data/rmsd/nanopore_vs_ox_rmsd.tsv", index=False, sep="\t")
 
     print("Performing many-vs-many RMSD calculations for Nanopore vs. TAB")
-    tab_result = many_vs_many_rmsd(nano_mb, tab_mb, '5hmC', [i for i in range(5, 25, 1)])
+    tab_result = many_vs_many_interassay(nano_mb, tab_mb, '5hmC', level_range)
     print(f"Average RMSD at 10x depth: {tab_result.loc[tab_result['Depth']==10, 'RMSD'].mean()}")
-    tab_result.to_csv("nanopore_vs_tab_rmsd.tsv", index=False, sep="\t")
-    # print(f"RMSE between Nanopore 5hmC and TAB: {}")
-        
-    # print(f"Interassay RMSE between oxBS 5mC: {nested_mean(rmse_comparer(oxbs_mb, oxbs_mb, 'mod', 'mod'))}")
-    # print(f"Interassay RMSE between TAB 5hmC: {nested_mean(rmse_comparer(tab_mb, tab_mb, 'mod', 'mod'))}")
+    tab_result.to_csv("data/rmsd/nanopore_vs_tab_rmsd.tsv", index=False, sep="\t")
     
     return print("Done.") 
 
@@ -121,13 +183,14 @@ if __name__=="__main__":
                         dest="test", 
                         required=False,
                         help="Whether a test output is produced.") 
+    parser.add_argument("--debug", 
+                        action="store_true", 
+                        dest="debug", 
+                        default=False,
+                        help="Whether a test output is produced.") 
 
     args = parser.parse_args()
-
-    global test
-    if args.test:
-        test = True
-    else: 
-        test = False
+    global test, debug
+    test, debug = args.test, args.debug
 
     main()    
