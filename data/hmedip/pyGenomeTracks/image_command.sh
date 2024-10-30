c4bf1c4e2ef018d783e875e69c2c983645987ab7
DominicOH <dominic.halliwell@hotmail.com>
2024-10-30 14:08:39 +0000

    Add data files for coverage comparison

---
+++ data/hmedip/pyGenomeTracks/image_command.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pyGenomeTracks --tracks hmedip_repImg1_tracks.ini --height 3.4 --width 8 --dpi 600 --fontSize 5 --trackLabelFraction 0.12 --region chr7:45,746,000-45,751,000 -o ../../../plots/hmedip_repImg1.svg
+
